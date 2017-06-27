e5071ae54be5638ed3ec34cd0b79788688afe75a
blazzII <blazzard@gmail.com>
2017-06-27 07:19:12 -0600

    Add project files.

---
+++ CIT365-Hello-World-Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CIT365_Hello_World_Assignment
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var name = "Brother Blazzard";
+            Console.WriteLine($"Hello {name}!");
+            // Using simple concatenation
+            Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine("Utah - United States of America");
+            var currentDateTime = DateTime.Now;
+            Console.WriteLine("Today is " + currentDateTime.ToString("dddd, MMMM dd, yyyy") + ".");
+        }
+    }
+}
